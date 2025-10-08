bf158be1f4a0f8eb426c3a78ae6c750d78940d3c
Aace <aacejovi@gmail.com>
2025-10-08 14:27:38 +0100

    form section

--- src/components/ui/Form.tsx
+++ src/components/ui/Form.tsx
@@ -1,5 +1,12 @@
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <section id="contact" className="my-20">
+      <h2 className="text-3xl lg:text-6xl text-white/80 text-center bbh-sans-bartle-regular px-6">
+        Let's make it happen
+      </h2>
+      <form></form>
+    </section>
+  );
 };
 
 export default Form;
